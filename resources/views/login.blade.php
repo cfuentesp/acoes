dd86abc0874a092633ea5b1e08d0b311a5b27958
cfuentesp <cfuentesp@unah.hn>
2022-10-18 18:52:35 -0600

    master

---
+++ resources/views/login.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere soluta magni quod obcaecati iste nobis in eum similique molestiae. Sed tempora illum consequatur expedita ea, nulla eveniet explicabo? In, quia!</h1>    
+</body>
+</html>
+
