41a6dff6bebc833689cfcd534d11246535ce0f2c
jeser.tablada@unah.hn <jeser.tablada@unah.hn>
2022-10-22 12:19:07 -0600

    cambios

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+<<<<<<< HEAD
 use App\Http\Controllers\PersonaController;
+
 use Illuminate\Support\Facades\Auth;
 
 
@@ -10,6 +12,7 @@ use Illuminate\Support\Facades\Auth;
 Route::view('/','welcome');
 Route::view('/login','login');
 Route::resource('personas','App\Http\Controllers\PersonaController');
+=======
 Route::view('/','login');
 
 Route::view('/registro','registro');
@@ -19,3 +22,4 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/logout', [\App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logoutt');
+>>>>>>> cfa762ceb7d827e44cedb394c5702771f02ced92
