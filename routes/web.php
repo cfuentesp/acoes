8d968409d8d2bc679dbf6d2cfa07ed8cae3a676c
cfuentesp <93753689+cfuentesp@users.noreply.github.com>
2022-10-26 19:36:28 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,49 @@
+<?php
 
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\inventarioController;
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+
+*/
+
+Route::view('/','login');
+
+Route::view('/registro','registro');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/logout', [\App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logoutt');
+
+// RUTAS INVENTARIO
+
+Route::get('/home/inventario', [App\Http\Controllers\inventarioController::class, 'getInventario'])->name('getListaEquipos');
+
+Route::get('/home/inventario/nuevo', [App\Http\Controllers\inventarioController::class, 'nuevoEquipo'])->name('nuevoEquipo');
+
+Route::get('/home/inventario/{id}', [App\Http\Controllers\inventarioController::class, 'getDatosEquipo'])->name('editarEquipo');
+
+Route::put('/home/inventario/{id}', [App\Http\Controllers\inventarioController::class, 'updateDatosEquipo'])->name('actualizarEquipo');
+
+//RUTAS MODULO PERSONAS
+Route::get('/home/persona', [App\Http\Controllers\HomeController::class, 'getPersona'])->name('home/persona');
+Route::get('/home/persona/nuevo', [App\Http\Controllers\PersonaController::class, 'nuevoPersona'])->name('nuevoPersona');
+
+Route::get('/home/persona/{id}', [App\Http\Controllers\PersonaController::class, 'getDatosPersona'])->name('editarPersona');
+
+Route::get('/home/persona/editar/hello/', [App\Http\Controllers\PersonaController::class, 'actualizar'])->name('actualizarPersona');
+
+//Route::resource('/personas','App\Http\Controllers\PersonaController');
